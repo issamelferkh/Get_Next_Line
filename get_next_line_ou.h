e7dbd087a7c0abaea39878057f42cbe9d7f3757a
Issam El ferkh <iel-ferk@e2r10p14.1337.ma>
2019-04-11 18:41:40 +0100

    beta

---
+++ get_next_line_ou.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iel-ferk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/09 20:11:59 by iel-ferk          #+#    #+#             */
+/*   Updated: 2019/04/09 20:12:02 by iel-ferk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+
+# define BUFF_SIZE 1024
+
+int				get_next_line(int const fd, char **line);
+
+#endif
