121a7a28a1bbc20665f521999b0f81f907a0398d
Issam El ferkh <iel-ferk@e2r10p15.1337.ma>
2019-04-14 05:07:31 +0100

    14/04 05:04

--- get_next_line.h
+++ get_next_line.h
@@ -1,22 +1,15 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: iel-ferk <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/11 19:02:51 by iel-ferk          #+#    #+#             */
-/*   Updated: 2019/04/12 00:27:13 by iel-ferk         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "../libft/libft.h"
-# include <sys/types.h>
-# include <sys/stat.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
 # include <fcntl.h>
-# define BUFF_SIZE 32
+# include <sys/types.h>
+# include <sys/uio.h>
+# define BUFF_SIZE 20
+
+int					get_next_line(const int fd, char **line);
 
 #endif
