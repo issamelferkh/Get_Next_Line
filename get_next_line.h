2c0b8e94318ee7de6e1782c1ff7df3b12e1f883b
Issam El ferkh <iel-ferk@e2r10p15.1337.ma>
2019-04-18 22:36:49 +0100

    18/04 22:35

--- get_next_line.h
+++ get_next_line.h
@@ -1,22 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iel-ferk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/09 20:11:59 by iel-ferk          #+#    #+#             */
+/*   Updated: 2019/04/09 20:12:02 by iel-ferk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <string.h>
-# include <stdlib.h>
-# include <unistd.h>
 # include <fcntl.h>
 # include <sys/types.h>
-# include <sys/uio.h>
-# define BUFF_SIZE 20
+# include <sys/stat.h>
 
-//int					get_next_line(const int fd, char **line);
-typedef	struct 		s_file
-{
-	int				fd;
-	char			*content_rest;
-	struct t_file	*next;
-}					t_file;
+# define BUFF_SIZE 9999
 
+int				get_next_line(int const fd, char **line);
 
 #endif
--- get_next_line_final.c
+++ get_next_line_final.c
@@ -27,44 +27,75 @@ static char	*ft_line_rest(char *tab)
 {
 	if (ft_strchr(tab, '\n'))
 	{
-		ft_strcpy(tab, ft_strchr(tab, '\n') + 1); //leak
+		ft_strcpy(tab, ft_strchr(tab, '\n') + 1);
 		return (tab);
 	}
 	if (ft_line_len(tab) > 0)
 	{
 		ft_strcpy(tab, ft_strchr(tab, '\0'));
-		//*tab = '\0';
 		return (tab);
 	}
 	return (NULL);
 }
+
+// static char	*ft_line_get(char *tab)
+// {
+// 	tab = ft_strrev(tab);
+// 	if (ft_strchr(tab, '\n') != NULL)
+// 	{
+// 		ft_strcpy(tab, ft_strchr(tab, '\n') + 1); // +1 pour (pas cpy)eviter \n 
+// 		tab = ft_strrev(tab);
+// 		return (tab);
+// 	}
+// 	if (ft_line_len(tab) > 0)
+// 	{
+// 		ft_strcpy(tab, ft_strchr(tab, '\0'));
+// 		tab = ft_strrev(tab);
+// 		return (tab);
+// 	}
+// 	return (NULL);
+// }
 
 int							get_next_line(int const fd, char **line)
 {
 	char					*tmp;
 	char					buff[BUFF_SIZE + 1];
 	static	char			*tab[4867]; 
-	int						r;
+	int						ret;
 
 	if (fd < 0 || BUFF_SIZE < 1 || !line || read(fd, buff, 0) < 0)
 		return (-1);
-	if (!(tab[fd]) && (tab[fd] = ft_strnew(0)) == NULL) //leak
+	if (!(tab[fd]) && (tab[fd] = ft_strnew(0)) == NULL)
 		return (-1);
-	while (!(ft_strchr(tab[fd], '\n')) && (r = read(fd, buff, BUFF_SIZE)) > 0)
+	while ((ft_strchr(tab[fd], '\n') == NULL) && (ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		buff[r] = '\0'; //for test
-		// printf("i am buff --> |%s|\n", buff);
+		buff[ret] = '\0'; //for test
+		printf("i am buff --> |%s|\n", buff);
 		tmp = tab[fd];
-		tab[fd] = ft_strnjoin(tmp, buff, r); // look to strjoin
-		// printf("i am tab[fd] --> |%s|\n", tab[fd]);
+		tab[fd] = ft_strnjoin(tmp, buff, ret);
+		printf("i am tab[fd] --> |%s|\n", tab[fd]);
 		free(tmp);
 	}
 	*line = ft_strsub(tab[fd], 0, ft_line_len(tab[fd]));
-	tmp = ft_line_rest(tab[fd]);
-	//free(tab[fd]);
-	return (tmp ? 1 : 0);
+	if (ft_line_rest(tab[fd]) == NULL)
+		return (0);
+	return (1);
 }
+/*******************************************************/
+// char 	*tab[3] = "0\0" 
+// char 	buff = "12345\0"
+// while ((ya pas "\n" in tab[3] && (read(fd, buff, BUFF_SIZE)) > 0)
+// {
+// char	*tmp = tab[3] = "0\0"
+// tab[3] = ft_strnjoin(tmp, buff, ret) = "0\0" + "12345\0" = "12345\0"
+// free(tmp); = tmp = NULL
+// So
+// 	char			*tmp = NULL
+// 	char			buff[BUFF_SIZE + 1] = "12345\0"
+// 	static	char	*tab[100] = "12345\0"
+// }
 
+/*******************************************************/
 int	main(int ac, char **av)
 {
 	int 	fd;
@@ -73,7 +104,7 @@ int	main(int ac, char **av)
 	fd = open(av[1], O_RDONLY);
 	while (get_next_line(fd, &line) == 1)
 	{
-		//printf("%s\n",line );
+		printf("%s\n",line );
 		//printf("%d\n",get_next_line(fd, &line));
 		free(line);
 	}
